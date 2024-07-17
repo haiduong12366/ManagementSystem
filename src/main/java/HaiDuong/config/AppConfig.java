fcada0cc96cc68bbed2d6e17e1ebfd6f0ee74097
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2024-07-17 16:33:12 +0700

    link

--- src/main/java/HaiDuong/config/AppConfig.java
+++ src/main/java/HaiDuong/config/AppConfig.java
@@ -49,7 +49,7 @@ public class AppConfig {
                         "http://localhost:4200/",
                         "http://localhost:3000/",
                         "http://localhost:5173/",
-                        "http://clever-compassion-production.up.railway.app"
+                        "https://quiet-bavarois-79fda8.netlify.app/"
                 ));
                 cfg.setAllowedMethods(Collections.singletonList("*"));//all method like post get ...
                 cfg.setAllowCredentials(true);
